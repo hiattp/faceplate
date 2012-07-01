bc9125052aba70504ffef5521980eb5eddea0d2d
Paul Hiatt <hiattp@gmail.com>
2012-07-01 16:30:05 -0400

    added cookie to maintain signed request

--- index.js
+++ index.js
@@ -13,14 +13,16 @@ var Faceplate = function(options) {
   this.secret  = this.options.secret;
 
   this.middleware = function() {
+    var cookieID = "fbsr_" + self.app_id;
     return function(req, res, next) {
       if (req.body.signed_request) {
+        if(req.session[cookieID] != req.body.signed_request) req.session[cookieID] = req.body.signed_request;
         self.parse_signed_request(req.body.signed_request, function(decoded_signed_request) {
           req.facebook = new FaceplateSession(self, decoded_signed_request);
           next();
         });
-      } else if (req.cookies["fbsr_" + self.app_id]) {
-        self.parse_signed_request(req.cookies["fbsr_" + self.app_id], function(decoded_signed_request) {
+      } else if (req.session[cookieID]) {
+        self.parse_signed_request(req.session[cookieID], function(decoded_signed_request) {
           req.facebook = new FaceplateSession(self, decoded_signed_request);
           next();
         });
